664072f3260b63cf56af1fe545d50aeb3aae708f
william widmer <wwidmer@hunter.cuny.edu>
2013-01-28 14:01:02 -0500

    Update Tag_Finder.JS/index.php

--- Tag_Finder.JS/index.php
+++ Tag_Finder.JS/index.php
@@ -1,12 +1,12 @@
-<? php
-
+<?php
+$q_GET["q"];
 // mysql connection
 
 $connect = mysql_connect("localhost", "user","password") or die(mysql_error());
 mysql_select_db("test", $connect) or die (mysql_error());
 
 
-$result = mysql_query("SELECT * FROM A WHERE Tag='x' AND Location='y') or die(mysql_error());
+$result = mysql_query("SELECT * FROM A WHERE Tag=$q") or die(mysql_error());
 $row = mysql_fetch($result);
 
 // echo to make sure it works
