11cf614bb8fa5ba23280735bab9a11f72aa67ba2
william widmer <wwidmer@hunter.cuny.edu>
2013-01-28 12:48:13 -0500

    Update Tag_Finder.JS/index.php

--- Tag_Finder.JS/index.php
+++ Tag_Finder.JS/index.php
@@ -5,7 +5,7 @@
 $connect = mysql_connect("localhost, "user","password") or die(mysql_error());
 mysql_select_db("databaseName", $connect) or die (mysql_error());
 
-// query
+
 
 $result = mysql_query("SELECT * FROM howTo WHERE ama='x' AND loc='y' AND need='z') or die(mysql_error());
 $row = mysql_fetch($result);
