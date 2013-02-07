f65a235d713501187d79fc62c75c4786193a88ae
william widmer <wwidmer@hunter.cuny.edu>
2013-02-07 13:23:40 -0500

    Update Tag_Finder.JS/index.php

--- Tag_Finder.JS/index.php
+++ Tag_Finder.JS/index.php
@@ -5,11 +5,11 @@ $r_GET["r"];
 // mysql connection
 
 $connect = mysql_connect("localhost", "root","") or die(mysql_error());
-mysql_select_db("db", $connect) or die (mysql_error());
+mysql_select_db("movementshowto", $connect) or die (mysql_error());
 
 // query the database, selects table a and finds tags (only tags for now; locations and subjects in the future)
 
-$query= "SELECT Name,URL FROM a WHERE Tag = 'A' ";
+$query= "SELECT Name,URL FROM howToTest WHERE Tag = '".$q."' and Topic = '".r."' ";
 
 $result = mysql_query($query) or die(mysql_error());
 
