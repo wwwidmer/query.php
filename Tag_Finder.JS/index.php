fde56e40a20cfcd9cd5bc386ee2766bc67b7735d
beaglew <williamwidmerr@gmail.com[user]>
2013-02-06 23:16:57 -0500

    2/6
    updated php file, now its been tested and works

--- Tag_Finder.JS/index.php
+++ Tag_Finder.JS/index.php
@@ -1,23 +1,19 @@
 <?php
 $q_GET["q"];
 $r_GET["r"];
+
 // mysql connection
 
-$connect = mysql_connect("localhost", "ODBC","password") or die(mysql_error());
-mysql_select_db("test", $connect) or die (mysql_error());
+$connect = mysql_connect("localhost", "root","") or die(mysql_error());
+mysql_select_db("db", $connect) or die (mysql_error());
 
 // query the database, selects table a and finds tags (only tags for now; locations and subjects in the future)
 
-$query= "SELECT * FROM a WHERE Tag = '".$q."' ";
+$query= "SELECT Name,URL FROM a WHERE Tag = 'A' ";
 
 $result = mysql_query($query) or die(mysql_error());
 
-// row is the array to send back to the page, which will render it as a list.
-
-while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
-    echo $row['URL'];
-    
-}
+echo $result;
   
 ?>
 
