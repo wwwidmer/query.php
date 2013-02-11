0a314d3732a94ae740274b882470c7208cd2baa4
william widmer <wwidmer@hunter.cuny.edu>
2013-02-11 11:57:06 -0500

    Update Tag_Finder.JS/tag_finder.php

--- Tag_Finder.JS/tag_finder.php
+++ Tag_Finder.JS/tag_finder.php
@@ -1,10 +1,10 @@
-<?php
+<php
+
 $q_GET["q"];
 $r_GET["r"];
 
-// mysql connection
 
-function mysqlconnect(){
+// mysql connection
 
 $host = "localhost";
 $user = "root";
@@ -14,36 +14,25 @@ $database = "movementshowto";
 $connect = mysql_connect($host,$user,$pw) or die(mysql_error());
 $query = mysql_select_db($database, $connect) or die (mysql_error());
 
-return $query;
-}
-
-
 // query the database, selects table a and finds tags and topics
 
-function query(){
 
-$query= "SELECT Name,URL FROM howToTest WHERE Tag = '".$q."' and Topic = '".r."' ";
+$query= "SELECT Name,URL FROM howToTest WHERE Tag = '".$q."' and Topic = '".$r."' ";
 
 $result = mysql_query($query) or die(mysql_error());
 
-while($row=mysql_fetch_array($result)){
-	echo $row['Name']. "-". $row['URL'];
-	echo "<br />";
 
-return $result;
-
+$data = array();
+while ( $row = mysql_fetch_row($result) )
+{
+    $data[] = $row;
 }
 
+echo json_encode($data);
 
-mysqlconnect();
-$res = query();
+  
+?>
 
-
-$array=mysql_fetch_row($res);
-echo json_encode($array);
-
-
-}
 
 
 
