565c1416579a47fc7e4e7cdb76f684be3552f9f1
beaglew <williamwidmerr@gmail.com[user]>
2013-02-10 21:21:06 -0500

    2/10

--- Tag_Finder.JS/tag_finder.php
+++ Tag_Finder.JS/tag_finder.php
@@ -4,17 +4,55 @@ $r_GET["r"];
 
 // mysql connection
 
-$connect = mysql_connect("localhost", "root","") or die(mysql_error());
-mysql_select_db("movementshowto", $connect) or die (mysql_error());
+function mysqlconnect(){
 
-// query the database, selects table a and finds tags (only tags for now; locations and subjects in the future)
+$host = "localhost";
+$user = "root";
+$pw = "";
+$database = "movementshowto";
+
+$connect = mysql_connect($host,$user,$pw) or die(mysql_error());
+$query = mysql_select_db($database, $connect) or die (mysql_error());
+
+return $query;
+}
+
+
+// query the database, selects table a and finds tags and topics
+
+function query(){
 
 $query= "SELECT Name,URL FROM howToTest WHERE Tag = '".$q."' and Topic = '".r."' ";
 
 $result = mysql_query($query) or die(mysql_error());
 
-$row = json_encode(mysql_fetch_assoc($result));
+while($row=mysql_fetch_array($result)){
+	echo $row['Name']. "-". $row['URL'];
+	echo "<br />";
 
-echo "queryResults(".$result.")";
+return $result;
+
+}
+
+
+mysqlconnect();
+$res = query();
+
+
+$array=mysql_fetch_row($res);
+echo json_encode($array);
+
+
+}
+
+
+
+
   
 ?>
+
+<html>
+<body>
+wdasdsad
+</body>
+</html>
